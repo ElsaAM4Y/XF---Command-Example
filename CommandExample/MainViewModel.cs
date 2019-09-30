331cbfaf428f0c50c84e9471973278a2e4ac7738
Elsa Kriesels <elsa_de_leeuwin@hotmail.com>
2019-09-30 14:35:03 +0200

    Command example XF with MVVM

---
+++ CommandExample/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace CommandExample
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            IncreaseCommand = new Command(IncreaseCount);
+
+            //if you want to execute the command manually then you do
+            // IncreaseCommand.Execute
+        }
+
+        public ICommand IncreaseCommand { get; }
+
+        int count = 0;
+
+        void IncreaseCount()
+        {
+            count++;
+            OnPropertyChanged(nameof(DisplayCount));
+        }
+
+        public string DisplayCount => $"You clicked {count} time(s).";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
